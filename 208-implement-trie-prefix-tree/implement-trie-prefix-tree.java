afad250fa21f5024310a2f81b75ec7f662077a09
Tanishq <122374798+tanishqchauhan2604@users.noreply.github.com>
2025-03-17 16:47:31 +0530

    Time: 38 ms (45.48%) | Memory: 55 MB (95.42%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,65 @@
+class Trie {
+
+    static class Node{
+        Node []link = new Node[26];
+        private boolean flag = false;
+        boolean containsKey(char ch){
+            return link[ch - 'a']!=null;
+        }
+        void put(char ch,Node node){
+            link[ch - 'a'] = node;
+        }
+        Node get(char ch){
+            return link[ch -'a'];
+        }
+        void setEnd(){flag = true;}
+        boolean isEnd(){return flag;}
+    }
+
+    private Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node.containsKey(word.charAt(i))){
+                node.put(word.charAt(i),new Node());
+            }
+            node = node.get(word.charAt(i));
+        }   
+        node.setEnd();
+    }
+    
+    public boolean search(String word) {
+        Node node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node.containsKey(word.charAt(i))){
+                return false;
+            }
+            node = node.get(word.charAt(i));
+        }
+        return node.isEnd();
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node node = root;
+        for(int i=0;i<prefix.length();i++){
+            if(!node.containsKey(prefix.charAt(i))){
+                return false;
+            }
+            node = node.get(prefix.charAt(i));
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
